df26e0ea8978101d69a8b76a7085217be3b4e2c9
Pazilye <pazi.jurat@outlook.com>
2025-01-12 22:13:35 +0100

    DATA1700 Project created Main language Javascript, later will be used Java and SpringBoot too

---
+++ src/main/java/org/example/data1700/Data1700Application.java
@@ -0,0 +1,13 @@
+package org.example.data1700;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data1700Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Data1700Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/data1700/Data1700ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.data1700;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data1700ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
